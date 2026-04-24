79550433a90f460c3ce2c0cb5df5408120cd6eba
die-gooo <diegorandazzo00@gmail.com>
2026-04-24 17:54:46 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.cityvibe.space" }],
-        destination: "https://cityvibe.space/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
-
 module.exports = nextConfig;
